028c0bdd573db1def61043e71b8d02198874a3dd
garlic0x1 <garlic1@protonmail.com>
2023-12-15 14:00:16 -0600

    set font size

--- config/misc.lisp
+++ config/misc.lisp
@@ -1,18 +1,24 @@
 (defpackage #:config/misc 
-  (:use :cl :lem :alexandria-2))
+  (:use :cl :lem :alexandria-2)
+  (:local-nicknames (:display :lem-sdl2/display)
+                    (:font :lem-sdl2/font)))
 (in-package :config/misc)
 
-;; bigger font
-(font-size-increase)
+(defun set-font-size (size)
+  (display:with-display (display)
+    (display:with-renderer (display)
+      (let ((font-config (display:display-font-config display)))
+        (display:change-font 
+         display 
+         (font:change-size font-config size))))))
+
+(set-font-size 18)
 
 ;; Disable Lem's auto recenter
 (setf *scroll-recenter-p* nil)
 
-(define-command open-init-file () ()
-  (line-up-first 
-   (lem-home)
-   (merge-pathnames "init.lisp")
-   find-file))
+(define-command open-config () ()
+  (line-up-first (lem-home) find-file))
 
 ;; Allow to suspend Lem by C-z.
 ;; It doesn't work well on Mac with Apple Silicon.
